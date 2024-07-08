421345c62da07e78136d9eebc009282a9ad90e8e
himanshuramteke007 <himanshuramteke007@gmail.com>
2024-07-08 17:48:16 +0000

    added problem 2

---
+++ conditionals/minimumamong3.js
@@ -0,0 +1,14 @@
+// let x = 30, y = 23, z = 55;
+let x = 30;
+let y = 23;
+let z = 55;
+
+if(x < y && x < z) { 
+    console.log(x);
+} 
+else if(y < x && y < z) { 
+    console.log(y);
+} 
+else {
+    console.log(z);
+}
