e96090f906b5ad850e7f48a947e5b573f64d997e
himanshuramteke007 <himanshuramteke007@gmail.com>
2024-07-11 17:11:54 +0000

    added pattern problem 4

---
+++ patterns/pattern4.js
@@ -0,0 +1,25 @@
+function pattern(n) {
+    for(let row = 1; row <= n; row += 1) {
+        let str = " ";
+        let spaces = n - row;
+        for(let j = 1; j <= spaces; j += 1) {
+            str += " ";
+        }
+        let stars = 2*row - 1;
+        for(let col = 1; col <= stars; col += 1) {
+            str += "*";
+        }
+        console.log(str);
+    }
+}
+
+pattern(5)
+
+/* output
+     *
+    ***
+   *****
+  *******
+ *********
+ 
+*/
