395baad777c1e6fa586895e7bb25c2796e315824
himanshuramteke007 <himanshuramteke007@gmail.com>
2024-07-13 11:23:42 +0000

    added gcd problem

---
+++ forloops/gcdProblem.js
@@ -0,0 +1,10 @@
+function gcd(a,b) {
+    let ans = 1;
+    for (let i = 2; i <= Math.min(a,b); i++) {
+        if (a % i == 0 && b % i == 0) {
+            ans = i;
+        }
+    }
+    return ans;
+}
+
